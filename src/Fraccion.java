f4ae550eaafeccd81631917a22d1cb4045defd60
EstebanGM1DAW <egonmorb@g.educaand.es>
2025-01-14 10:56:43 +0100

    tercera

--- src/Fraccion.java
+++ src/Fraccion.java
@@ -3,38 +3,38 @@ public class Fraccion {
     int denominador;
     int numerador;
     //getters
-    int getDenominador() {
+    public int getDenominador() {
         return denominador;
     }
-    int getNumerador(){
+    public int getNumerador(){
         return numerador;
     }
     //setters
-    void setFraccion(int den, int num){
+    public void setFraccion(int den, int num){
         denominador = den;
         numerador = num;
     }
     //metodos
-    void invertir(){
+    public void invertir(){
         int a = numerador;
         numerador = denominador;
         denominador = a;
     }
-    Fraccion multiplicar(Fraccion f1, Fraccion f2){
+    public Fraccion multiplicar(Fraccion f1, Fraccion f2){
         Fraccion f3 = new Fraccion();
         int den = f1.getDenominador()*f2.getDenominador();
         int num = f1.getNumerador()*f2.getNumerador();
         f3.setFraccion(den, num);
         return f3;
     }
-    Fraccion dividir(Fraccion f1, Fraccion f2){
-        Fraccion f3 = new Fraccion();
+    public static Fraccion dividir(Fraccion f1, Fraccion f2){
+        Fraccion resultado = new Fraccion();
         int den = f1.getNumerador()*f2.getDenominador();
         int num = f1.getDenominador()*f2.getNumerador();
-        f3.setFraccion(num,den);
-        return f3;
+        resultado.setFraccion(num,den);
+        return resultado;
     }
-    void simplificar(){
+    public void simplificar(){
         do {
         if (numerador%2==0 && denominador%2==0) {
             numerador = numerador/2;
