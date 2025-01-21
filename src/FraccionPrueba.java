6a07db6ab4a044ac255cb7804e681bf5f183cf98
EstebanGM1DAW <egonmorb@g.educaand.es>
2025-01-21 10:02:36 +0100

    octava

--- src/FraccionPrueba.java
+++ src/FraccionPrueba.java
@@ -2,7 +2,8 @@ public class FraccionPrueba {
     public static void main(String[] args) {
         Fraccion f1 = new Fraccion();
         f1.setFraccion(1344, 360);
+        System.out.println("Antes de simplificar: "+f1.getDenominador() + " " + f1.getNumerador());
         Fraccion.simplificar(f1);
-        System.out.println(f1.getDenominador() + " " + f1.getNumerador());
+        System.out.println("Despues de simplificar: "+f1.getDenominador() + " " + f1.getNumerador());
     }
 }
